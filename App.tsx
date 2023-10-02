3f6ca401e61e2934f014727d6969afbea7237553
Jakub Pacewicz <pecet3107@gmail.coim>
2023-10-02 21:49:49 +0200

    .

--- App.tsx
+++ App.tsx
@@ -1,10 +1,22 @@
 import React, { useState } from 'react'
 import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
 import { Feather } from '@expo/vector-icons'
-import CurrentWeather from './CurrentWeather'
+import CurrentWeather from './components/CurrentWeather'
+import UpcommingWeather from './components/UpcommingWeather'
 
 const App = () => {
-  return <CurrentWeather />
+  return (
+    <View style={styles.container}>
+      <CurrentWeather />
+      <UpcommingWeather />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
 
 export default App
