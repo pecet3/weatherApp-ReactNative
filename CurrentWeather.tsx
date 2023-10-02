4bca2b7d0d6d87c21203a77d1abf84b80967490d
Jakub Pacewicz <pecet3107@gmail.coim>
2023-10-02 18:58:20 +0200

    splited component

---
+++ CurrentWeather.tsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+import { View, Text, SafeAreaView, StyleSheet } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+
+const CurrentWeather = () => {
+  const [temperature, setTemperature] = useState(6)
+  const [feelTemperature, setFeelTemperature] = useState(5)
+  const [maxTemperature, setMaxTemperature] = useState(8)
+  const [minTemperature, setMinTemperature] = useState(4)
+  return (
+    <SafeAreaView style={styles.wrapper}>
+      <View style={styles.container}>
+        <Text style={styles.largeText}>Current weather</Text>
+        <Feather name="sun" size={32} color="black" />
+        <Text style={styles.mediumText}>Temperature:{temperature}</Text>
+        <Text style={(styles.smallText, { color: '#1c89dc' })}>
+          Feels like: {feelTemperature}
+        </Text>
+        <View style={styles.rowContainer}>
+          <Text style={styles.smallText}>max:{maxTemperature}</Text>
+          <Text style={styles.smallText}>min:{minTemperature}</Text>
+        </View>
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.largeText}>It's Sunny</Text>
+        <Text style={styles.mediumText}>
+          You can wear a hoodie and bring some jacket with you
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#d3d3d3',
+    flex: 1,
+    alignItems: 'center',
+    gap: 8
+  },
+  bottomContainer: {
+    backgroundColor: '#898989',
+    padding: 2,
+    alignItems: 'center'
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    gap: 4
+  },
+  wrapper: {
+    flex: 1,
+    paddingTop: 30
+  },
+  largeText: {
+    fontSize: 26
+  },
+  mediumText: {
+    fontSize: 20
+  },
+  smallText: {
+    fontSize: 16
+  }
+})
+export default CurrentWeather
