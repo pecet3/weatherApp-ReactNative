d77d38570e73fece72dd042d5a893ea3ee671ec9
Jakub Pacewicz <pecet3107@gmail.coim>
2023-10-02 18:34:49 +0200

    .

--- App.tsx
+++ App.tsx
@@ -5,7 +5,9 @@ const App = () => {
   return (
     <SafeAreaView style={styles.wrapper}>
       <View style={styles.container}>
-        <Text>Current weather</Text>
+        <Text style={styles.h1}>Current weather</Text>
+        <Text style={styles.temp}>Temperature: 6</Text>
+        <Text style={styles.tempFeels}>Feels like: 5</Text>
       </View>
     </SafeAreaView>
   )
@@ -15,10 +17,22 @@ const styles = StyleSheet.create({
   container: {
     backgroundColor: '#d3d3d3',
     flex: 1,
-    alignItems: 'center'
+    alignItems: 'center',
+    gap: 8
   },
   wrapper: {
-    flex: 1
+    flex: 1,
+    paddingTop: 30
+  },
+  h1: {
+    fontSize: 25
+  },
+  temp: {
+    fontSize: 20
+  },
+  tempFeels: {
+    fontSize: 18,
+    color: '#1c89dc'
   }
 })
 export default App
